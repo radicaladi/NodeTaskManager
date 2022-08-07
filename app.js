78198728a67297bb0d563af4628522dc949f80d4
Adrian Brown <adrian.r.brown90@gmail.com>
2022-08-06 17:17:33 -0700

    adds: dotenv pkg for security

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require('./db/connect');
-const {connect} = require("mongoose");
+require('dotenv').config() //passes to start fn
+
 // middleware
 app.use(express.json());
 
@@ -24,7 +25,7 @@ const port = 3000;
 
 const start = async ()=>{
     try {
-        await connectDB(); // try connection to DB, if successful, spin up server -->
+        await connectDB(process.env.MONGO_URI); // try connection to DB (passes pw pkg + var), if successful, spin up server -->
         app.listen(port, console.log(`Server listening on port ${port}...`));
     }
     catch (error) {
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-    'mongodb+srv://Adrian:m0ngoose@nodetaskmanager.q0idbfy.mongodb.net/NodeTaskManager?retryWrites=true&w=majority';
-
 const connectDB = (url) =>{
     return mongoose.connect(connectionString)
 }
