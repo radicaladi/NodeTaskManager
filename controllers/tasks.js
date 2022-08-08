38491697866c7c26583dfb38adec198893e53fc9
Adrian Brown <adrian.r.brown90@gmail.com>
2022-08-07 19:24:41 -0700

    adds: updateTask fn async/await try-catch block for updating a task to the db and return new version afterwards

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -21,7 +21,7 @@ const createTask = async (req, res) => {
 
 const getTask = async (req, res) => {
     try {
-        const {id:taskID} = req.params;
+        const { id: taskID } = req.params;
         const task = await Task.findOne({_id:taskID});
         if (!task) {
             return res.status(404).json({ msg:`No task with id : ${taskID}`})  // always setup return to ensure doesn't send task twice
@@ -33,13 +33,9 @@ const getTask = async (req, res) => {
 
 }
 
-const updateTask = (req, res) => {
-    res.send('update task')
-}
-
 const deleteTask = async (req, res) => {
     try {
-        const {id:taskID} = req.params;
+        const { id: taskID } = req.params;
         const task = await Task.findOneAndDelete({_id:taskID});
         if (!task) {
             return res.status(404).json({ msg:`No task with id : ${taskID}`})  // always setup return to ensure doesn't send task twice
@@ -49,6 +45,23 @@ const deleteTask = async (req, res) => {
        res.status(500).json({ msg: error });
     }
     res.send('delete task')
+}
+
+const updateTask = async (req, res) => {
+    try {
+        const { id:taskID } = req.params;
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new: true,
+            runValidators: true,
+        }); // 3rd param is for options to retain new value after update
+        if(!task) {
+            return res.status(404).json({ msg:`No task with id : ${taskID}`});  // always setup return to ensure doesn't send task twice
+        }
+        res.status(200).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+    res.send('update task')
 }
 
 module.exports = {
